b0baa6d817eaa6a6128e61afa0441241faa2a91c
sisou <hello@soerenschwert.de>
2018-07-03 19:28:17 +0200

    Push Nimiq.d.ts WIP

---
+++ Nimiq.d.ts
@@ -0,0 +1,13 @@
+declare namespace Nimiq {
+    const Address: any
+    const BufferUtils: any
+    const KeyPair: any
+    const SerialBuffer: any
+    const Signature: any
+    const Transaction: any
+    const BasicTransaction: any
+    const ExtendedTransaction: any
+    const Account: any
+    const SignatureProof: any
+    const PublicKey: any
+}
