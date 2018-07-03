655e577624622300263895e5c41c7038402ee5b3
Don Maxito <maxharz@gmail.com>
2018-07-04 00:05:40 +0200

    draft for upgrade process

--- Nimiq.d.ts
+++ Nimiq.d.ts
@@ -1,13 +1,36 @@
 declare namespace Nimiq {
+    type Address = any
     const Address: any
+
     const BufferUtils: any
+
+    type KeyPair = any
     const KeyPair: any
+
+    type SerialBuffer = any
     const SerialBuffer: any
-    const Signature: any
+
+    /*type Signature = {
+        create() : Signature
+    }*/
+    type Signature = any
+    const Signature : any
+
+    type Transaction = any
     const Transaction: any
+
+    type BasicTransaction = Transaction & { }
     const BasicTransaction: any
+
     const ExtendedTransaction: any
+
     const Account: any
+
     const SignatureProof: any
+
+    type PublicKey = any
     const PublicKey: any
 }
+
+type TransactionFormat = 'basic' | 'extended'
+
