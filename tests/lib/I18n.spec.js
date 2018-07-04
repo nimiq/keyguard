9e634ff06bde8d90815c579895fcad7ddad4f080
sisou <hello@soerenschwert.de>
2018-07-05 00:20:56 +0200

    Rename according to convention

--- tests/lib/I18n.spec.js
+++ tests/lib/I18n.spec.js
@@ -1,22 +1,22 @@
-describe("i18n test suite", function () {
+describe("I18n", function () {
 
     const i18n = new I18n(TRANSLATIONS);
 
-    it("i18n dict has languages", function () {
+    it("dictionary has languages", function () {
         const languages = i18n.availableLanguages();
         expect(languages).not.toBeNull();
         expect(languages.length).toBeGreaterThan(0);
         expect(languages).not.toContain('');
     });
 
-    it("i18n dict languages have labels", function () {
+    it("dictionary languages have labels", function () {
         for (const language of i18n.availableLanguages()) {
             expect(i18n.dictionary[language]).toBeDefined();
             expect(i18n.dictionary[language]._language).toBeDefined();
         }
     });
 
-    it("i18n dict entries are complete", function () {
+    it("dictionary entries are complete", function () {
         const BASELINE = 'en';
         const english = i18n.dictionary[BASELINE];
         const languages = i18n.availableLanguages();
