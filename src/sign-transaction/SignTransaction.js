af45b1a7164359fa238c646494a1bee073ee5f04
Don Maxito <maxharz@gmail.com>
2018-07-04 18:21:46 +0200

    use Observable and register on result for resolving request

--- src/sign-transaction/SignTransaction.js
+++ src/sign-transaction/SignTransaction.js
@@ -1,32 +1,29 @@
 class Api {
     async request(transaction) {
-        // get key from keystore
-        const keyType = await keyStore.getType(transaction.sender);
-        if (keyType === EncryptionType.HIGH) {
-            // start UI
 
-        } else {
-            // start UI
-
-        }
-    }
-
-    async signWithPassphrase(transaction) {
-        /*if (transaction.value < 1 / Nimiq.Policy.SATOSHIS_PER_COIN) {
+        if (transaction.value < 1 / Nimiq.Policy.SATOSHIS_PER_COIN) {
             throw new Error('Amount is too small');
         }
-        if (transaction.network !== Nimiq.GenesisConfig.NETWORK_NAME) throw Error(`Network missmatch: ${transaction.network} in transaction, but ${Nimiq.GenesisConfig.NETWORK_NAME} in Keyguard`);
 
-        const key = await keyStore.getPlain(transaction.sender);
-        if (key.type !== KeyType.HIGH) throw new Error('Unauthorized: sender is not a Safe account');
+        if (transaction.network !== Nimiq.GenesisConfig.NETWORK_NAME) {
+            throw Error(`Network missmatch: ${transaction.network} in transaction, but ${Nimiq.GenesisConfig.NETWORK_NAME} in Keyguard`);
+        }
 
-        transaction.value = Nimiq.Policy.coinsToSatoshis(transaction.value);
-        transaction.fee = Nimiq.Policy.coinsToSatoshis(transaction.fee);
+        // get key from keystore
+        const keyStore = KeyStore.instance;
+        const keyType = await keyStore.getType(transaction.sender);
 
-        return this._startRequest(RequestTypes.SIGN_SAFE_TRANSACTION, {
-            transaction,
-            address: transaction.sender
-        });*/
+        return new Promise((resolve, reject) => {
+
+            if (keyType === EncryptionType.HIGH) {
+                const signTransactionSafe = new SignTransactionSafe(transaction);
+
+                signTransactionSafe.on('result', result => resolve(result));
+                signTransactionSafe.on('error', error => reject(error));
+            } else {
+                // start UI
+            }
+        });
     }
 }
 
