737305a3914c69979666dcafa7e9fb98e6ca8ab9
danimoh <github.danimoh@neverbox.com>
2018-07-06 12:11:11 +0200

    karma: fix type scripting

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration
 
-module.exports = function (config) {
+module.exports = function (/** @type {any} */ config) {
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
