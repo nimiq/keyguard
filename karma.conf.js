9ea256b0e9ccc9cc7165b2dbce10d9a0485e6a46
Don Maxito <maxharz@gmail.com>
2018-07-11 15:59:53 +0200

    play around with istanbuljs (wip)

--- karma.conf.js
+++ karma.conf.js
@@ -26,15 +26,17 @@ module.exports = function (/** @type {any} */ config) {
         ],
 
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
-
-
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress'], //, 'coverage'],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            //'src/**/*.js': ['coverage']
+        },
 
 
         // web server port
