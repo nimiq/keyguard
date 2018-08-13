e943cb16595921d04566aa325c0f1bdd438a4085
sisou <hello@soerenschwert.de>
2018-08-13 10:27:37 +0200

    Format transaction value

--- src/request/sign-transaction/BaseLayout.js
+++ src/request/sign-transaction/BaseLayout.js
@@ -63,12 +63,9 @@ class BaseLayout {
         // Set value and fee.
         const total = transaction.value + transaction.fee;
         const totalNim = Nimiq.Policy.satoshisToCoins(total);
-        let totalNimFormatted = '';
 
-        if (parseFloat(totalNim.toFixed(2)) === totalNim) totalNimFormatted = totalNim.toFixed(2);
-        else totalNimFormatted = totalNim.toString();
+        $value.textContent = this._formatNumber(totalNim);
 
-        $value.textContent = totalNimFormatted;
         if ($fee && transaction.fee > 0) {
             $fee.textContent = Nimiq.Policy.satoshisToCoins(transaction.fee).toString();
             /** @type {HTMLDivElement} */
@@ -170,6 +167,27 @@ class BaseLayout {
         // Go to start page
         window.location.hash = BaseLayout.Pages.CONFIRM_TRANSACTION;
         this._passphraseInput.focus();
+    }
+
+    /**
+     * @param {number} value
+     * @param {number} [maxDecimals]
+     * @param {number} [minDecimals]
+     * @returns {string}
+     */
+    _formatNumber(value, maxDecimals = 5, minDecimals = 2) {
+        const roundingFactor = 10 ** maxDecimals;
+        value = Math.floor(value * roundingFactor) / roundingFactor;
+
+        const result = parseFloat(value.toFixed(minDecimals)) === value
+            ? value.toFixed(minDecimals)
+            : value.toString();
+
+        if (Math.abs(value) < 10000) return result;
+
+        // Add thin spaces (U+202F) every 3 digits. Stop at the decimal separator if there is one.
+        const regexp = minDecimals > 0 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(\d{3})+$)/g;
+        return result.replace(regexp, '$1\u202F');
     }
 }
 
