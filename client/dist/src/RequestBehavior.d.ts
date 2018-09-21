2b9b7bf4b8126fa865d10fb02a1ba04ebfbde794
sisou <hello@soerenschwert.de>
2018-09-21 21:56:14 +0200

    Ignore client dist files

--- client/dist/src/RequestBehavior.d.ts
+++
@@ -1,9 +0,0 @@
-import { KeyguardCommand } from './RequestTypes';
-export declare class RequestBehavior {
-    static getAllowedOrigin(endpoint: string): string;
-    static getRequestUrl(endpoint: string, command: KeyguardCommand): string;
-    private _targetUrl;
-    private _localState;
-    constructor(targetUrl?: string, localState?: any);
-    request(endpoint: string, command: KeyguardCommand, args: any[]): Promise<void>;
-}
--- client/dist/src/main.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './KeyguardClient';
-export * from './RequestBehavior';
-export * from './RequestTypes';
