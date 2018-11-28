833411cb4033b242acec040940eb1d4b8c8c8921
Sebastian <sebastian@nimiq.com>
2018-11-28 11:03:11 -0600

    simplification

--- src/request/change-passphrase/ChangePassphrase.js
+++ src/request/change-passphrase/ChangePassphrase.js
@@ -41,46 +41,63 @@ class ChangePassphrase {
             },
         );
         this._setPassphraseBox = new PassphraseSetterBox($setPassphraseeBox);
-        this._enterPassphraseBox.on(PassphraseBox.Events.SUBMIT, async phrase => {
-            document.body.classList.add('loading');
-            try {
-                const passphrase = phrase ? Nimiq.BufferUtils.fromAscii(phrase) : undefined;
-                const key = await KeyStore.instance.get(this._request.keyInfo.id, passphrase);
-                if (!key) {
-                    this._reject(new Error('No key'));
-                    return;
-                }
-                this._key = key;
-                this._setPassphraseBox.reset();
-                window.location.hash = ChangePassphrase.Pages.SET_PASSPHRASE;
-                this._setPassphraseBox.focus();
-                document.body.classList.remove('loading');
-            } catch (e) {
-                if (e.message === 'Invalid key') {
-                    document.body.classList.remove('loading');
-                    this._enterPassphraseBox.onPassphraseIncorrect();
-                } else this._reject(e);
-            }
-        });
-
-        this._setPassphraseBox.on(
-            PassphraseSetterBox.Events.SUBMIT,
-            /** @param {string} passphrase */ passphrase => {
-                document.body.classList.add('loading');
-                this._passphrase = passphrase;
-                this._finish();
-            },
-        );
-
-        this._setPassphraseBox.on(PassphraseSetterBox.Events.SKIP, () => {
-            this._finish();
-        });
+        this._enterPassphraseBox.on(PassphraseBox.Events.SUBMIT, this._passphraseSubmitted.bind(this));
+        this._setPassphraseBox.on(PassphraseSetterBox.Events.SUBMIT, this._finish.bind(this));
+        this._setPassphraseBox.on(PassphraseSetterBox.Events.SKIP, this._finish.bind(this));
     }
 
     run() {
         this._enterPassphraseBox.reset();
         window.location.hash = ChangePassphrase.Pages.ENTER_PASSPHRASE;
         this._enterPassphraseBox.focus();
+    }
+
+    /**
+     * @param {string} phrase
+     */
+    async _passphraseSubmitted(phrase) {
+        document.body.classList.add('loading');
+        try {
+            const passphrase = phrase ? Nimiq.BufferUtils.fromAscii(phrase) : undefined;
+            const key = await KeyStore.instance.get(this._request.keyInfo.id, passphrase);
+            if (!key) {
+                this._reject(new Error('No key'));
+                return;
+            }
+            this._key = key;
+            this._setPassphraseBox.reset();
+            window.location.hash = ChangePassphrase.Pages.SET_PASSPHRASE;
+            this._setPassphraseBox.focus();
+            document.body.classList.remove('loading');
+        } catch (e) {
+            if (e.message === 'Invalid key') {
+                document.body.classList.remove('loading');
+                this._enterPassphraseBox.onPassphraseIncorrect();
+            } else this._reject(e);
+        }
+    }
+
+    /**
+     * @param {string} phrase
+     */
+    async _finish(phrase) {
+        document.body.classList.add('loading');
+        if (!this._key) {
+            this._reject(new Error('Bypassed Password'));
+            return;
+        }
+
+        // In this request, the user can only set a new password (min length: 8) or leave a key unencrypted.
+        // In any case, the key is not encrypted with a 6-digit PIN anymore.
+        this._key.hasPin = false;
+
+        const passphrase = this._passphrase.length > 0 ? Nimiq.BufferUtils.fromAscii(phrase) : undefined;
+        await KeyStore.instance.put(this._key, passphrase);
+
+        const result = {
+            success: true,
+        };
+        this._resolve(result);
     }
 
     _buildEnterPassphrasePage() {
@@ -132,25 +149,6 @@ class ChangePassphrase {
         const $app = (document.getElementById('app'));
         $app.insertBefore($el, $app.children[1]);
         return $el;
-    }
-
-    async _finish() {
-        if (!this._key) {
-            this._reject(new Error('Bypassed Password'));
-            return;
-        }
-
-        // In this request, the user can only set a new password (min length: 8) or leave a key unencrypted.
-        // In any case, the key is not encrypted with a 6-digit PIN anymore.
-        this._key.hasPin = false;
-
-        const passphrase = this._passphrase.length > 0 ? Nimiq.BufferUtils.fromAscii(this._passphrase) : undefined;
-        await KeyStore.instance.put(this._key, passphrase);
-
-        const result = {
-            success: true,
-        };
-        this._resolve(result);
     }
 }
 
