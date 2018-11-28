9ca8402ca751eaafb12fe6adea21091234a2f5e6
Sebastian <sebastian@nimiq.com>
2018-11-28 16:18:01 -0600

    return to Accounts to switch from Import to Create

--- src/request/import/ImportApi.js
+++ src/request/import/ImportApi.js
@@ -75,7 +75,7 @@ class ImportApi extends TopLevelApi {
             );
             handler.run();
         });
-        $createWalletLink.addEventListener('click', () => alert('Sorry, not implemented yet.'));
+        $createWalletLink.addEventListener('click', () => this.reject(new Error('GO_CREATE')));
 
         return {
             passphraseBox,
