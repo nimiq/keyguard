204c64abe0e0ba3a6f3413b0ce9f1a16bbe623aa
sisou <hello@soerenschwert.de>
2018-11-28 20:56:11 -0600

    Update WalletFile design

--- src/lib/WalletFile.js
+++ src/lib/WalletFile.js
@@ -63,15 +63,28 @@ class WalletFile {
      * @param {string} encodedPrivKey
      * @returns {Promise<void>}
      */
-    _draw(address, encodedPrivKey) {
-        this._drawBackgroundGradient();
+    async _draw(address, encodedPrivKey) {
+        this._drawBackground();
         this._drawEncodedPrivKey(encodedPrivKey);
 
-        this._setFont();
+        // this._setFont();
         this._drawAddress(address);
-        this._drawHeader();
+        this._drawHeaderText();
+        this._drawWarningText();
 
-        return this._drawIdenticon(address);
+        await this._drawNimiqLogo();
+        await this._drawIdenticon(address);
+    }
+
+    async _drawNimiqLogo() {
+        const $img = new Image();
+        const loaded = new Promise(resolve => {
+            $img.onload = () => resolve();
+        });
+        // eslint-disable-next-line max-len
+        $img.src = 'data:image/svg+xml,<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 270 64"><defs><style>.cls-1{fill:url(%23radial-gradient);}.cls-2{fill:%231f2348;}</style><radialGradient id="radial-gradient" cx="54.17" cy="63.17" r="72.02" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="%23ec991c"/><stop offset="1" stop-color="%23e9b213"/></radialGradient></defs><title>nimiq_logo_rgb_horizontal</title><path class="cls-1" d="M71.2,29l-15-26A6,6,0,0,0,51,0H21a6,6,0,0,0-5.19,3L.8,29a6,6,0,0,0,0,6l15,26A6,6,0,0,0,21,64H51a6,6,0,0,0,5.19-3l15-26A6,6,0,0,0,71.2,29Z"/><path class="cls-2" d="M124.5,13H131V51h-5L105.5,24.5V51H99V13h5l20.5,26.5Z"/><path class="cls-2" d="M144,51V13h7V51Z"/><path class="cls-2" d="M195.5,13H201V51h-6V27.5L184.75,51h-4.5L170,27.5V51h-6V13h5.5l13,29.75Z"/><path class="cls-2" d="M214,51V13h7V51Z"/><path class="cls-2" d="M267.76,42.34a15.86,15.86,0,0,1-8,7.59,21.09,21.09,0,0,0,3.05,4.1,38,38,0,0,0,4,3.72L262,61.5a29,29,0,0,1-4.82-4.38,31.82,31.82,0,0,1-4.07-5.67c-.28,0-1.1,0-1.61,0a19.67,19.67,0,0,1-9.89-2.36,15.71,15.71,0,0,1-6.4-6.8A23.51,23.51,0,0,1,233,32a22.34,22.34,0,0,1,2.24-10.34,15.87,15.87,0,0,1,6.42-6.8,21.8,21.8,0,0,1,19.73,0,15.71,15.71,0,0,1,6.4,6.8A22.6,22.6,0,0,1,270,32,23.33,23.33,0,0,1,267.76,42.34Zm-24.73,0a11.7,11.7,0,0,0,16.94,0c2-2.43,3-5.85,3-10.37s-1-8-3-10.39a11.78,11.78,0,0,0-16.94,0c-2,2.39-3,5.9-3,10.42S241,39.94,243,42.37Z"/></svg>';
+        await loaded;
+        this._ctx.drawImage($img, WalletFile.PADDING * 4, WalletFile.PADDING * 4, 150, 36);
     }
 
     /**
@@ -82,25 +95,33 @@ class WalletFile {
         const size = WalletFile.IDENTICON_SIZE;
         const pad = (this._width - size) / 2;
         const x = pad;
-        const y = this._height - this._width - size / 2;
+        const y = this._height - this._width - size / 1.5;
         this._ctx.drawImage($img, x, y, size, size);
     }
 
-    _setFont() {
+    // _setFont() {
+    //     const ctx = this._ctx;
+    //     ctx.textAlign = 'center';
+    // }
+
+    _drawHeaderText() {
         const ctx = this._ctx;
-        ctx.textAlign = 'center';
+        const x = 150 + 32 + 8;
+        const y = WalletFile.PADDING * 7.6;
+        ctx.font = `400 30px ${WalletFile.FONT_FAMILY}`;
+        ctx.textAlign = 'left';
+        ctx.fillStyle = 'rgb(31, 35, 72, 1)'; // Based on Nimiq Blue
+        ctx.fillText('Wallet File', x, y);
     }
 
-    _drawHeader() {
+    _drawWarningText() {
         const ctx = this._ctx;
-        const x = this._width / 2;
-        const y = WalletFile.PADDING * 6;
-        ctx.font = `600 20px ${WalletFile.FONT_FAMILY}`;
-        ctx.fillStyle = 'rgba(255,255,255,0.7)';
-        ctx.fillText('ACCOUNT ACCESS FILE', x, y);
-
+        const x = WalletFile.WIDTH / 2;
+        const y = WalletFile.HEIGHT - WalletFile.PADDING * 2.5;
         ctx.font = `400 16px ${WalletFile.FONT_FAMILY}`;
-        ctx.fillText('DO NOT share this File or QR Code!', x, WalletFile.PADDING * 12.5);
+        ctx.textAlign = 'center';
+        ctx.fillStyle = 'rgb(255, 255, 255, 0.7)'; // Based on Nimiq Blue
+        ctx.fillText('Do not share this file or QR Code', x, y);
     }
 
     /**
@@ -109,9 +130,10 @@ class WalletFile {
     _drawAddress(address) {
         const ctx = this._ctx;
         const x = this._width / 2;
-        const y = this._width;
+        const y = this._width / 1.2;
         ctx.font = `400 16px ${WalletFile.FONT_FAMILY}`;
-        ctx.fillStyle = 'white';
+        ctx.textAlign = 'center';
+        ctx.fillStyle = 'rgb(31, 35, 72, 0.7)'; // Based on Nimiq Blue
         ctx.fillText(address, x, y);
     }
 
@@ -124,7 +146,7 @@ class WalletFile {
             text: encodedPrivKey,
             radius: 0.8,
             ecLevel: 'Q',
-            fill: '#2e0038',
+            fill: '#1F2348',
             background: 'transparent',
             size: Math.min(240, (window.innerWidth - 64)),
         });
@@ -134,23 +156,37 @@ class WalletFile {
         const qrPosition = WalletFile.calculateQrPosition(this._width, this._height);
 
         this._ctx.fillStyle = 'white';
-        this._ctx.strokeStyle = 'white';
-        this._roundRect(qrPosition.x, qrPosition.y, qrPosition.size, qrPosition.size, 16, true);
+        this._roundRect(qrPosition.x, qrPosition.y, qrPosition.size,
+            qrPosition.size, WalletFile.RADIUS / 2, true, false);
 
         const padding = qrPosition.padding;
         this._ctx.drawImage($canvas, qrPosition.x + padding, qrPosition.y + padding, qrPosition.size - 2 * padding,
             qrPosition.size - 2 * padding);
     }
 
-    _drawBackgroundGradient() {
-        this._ctx.fillStyle = 'white';
-        this._ctx.fillRect(0, 0, this._width, this._height);
-        const gradient = this._ctx.createLinearGradient(0, 0, 0, this._height);
-        gradient.addColorStop(0, '#536DFE');
-        gradient.addColorStop(1, '#a553fe');
-        this._ctx.fillStyle = gradient;
-        this._ctx.strokeStyle = 'transparent';
-        this._roundRect(0, 0, this._width, this._height, 16, true);
+    _drawBackground() {
+        const ctx = this._ctx;
+
+        ctx.fillStyle = 'transparent';
+        ctx.fillRect(0, 0, this._width, this._height);
+
+        // const gradient = ctx.createLinearGradient(0, 0, 0, this._height);
+        // gradient.addColorStop(0, '#F4F4F5');
+        // gradient.addColorStop(1, '#F4F4F4');
+        ctx.fillStyle = '#FAFAFA';
+        this._roundRect(0, 0, this._width, this._height, WalletFile.RADIUS, true, false);
+
+        // Draw dark bottom shape
+        ctx.fillStyle = '#1F2348';
+        this._roundRect(0, this._height - 275, this._width, 275, {
+            tl: 0,
+            tr: 0,
+            br: WalletFile.RADIUS,
+            bl: WalletFile.RADIUS,
+        }, true, false, {
+            tl: { x: 0, y: 0 },
+            tr: { x: 0, y: -48 },
+        });
     }
 
     /**
@@ -158,25 +194,37 @@ class WalletFile {
      * @param {number} y
      * @param {number} width
      * @param {number} height
-     * @param {number} radius
+     * @param {number|{tl: number, tr: number, br: number, bl: number}} radius
      * @param {boolean} fill
      * @param {boolean} stroke
+     * @param {{tl: {x: number, y: number}, tr: {x: number, y: number}}} [skew]
      */
-    _roundRect(x, y, width, height, radius = 5, fill = false, stroke = true) {
+    _roundRect(x, y, width, height, radius = 5, fill = false, stroke = true, skew) {
         const ctx = this._ctx;
-        const radiusObj = {
-            tl: radius, tr: radius, br: radius, bl: radius,
-        };
+
+        /** @type {{tl: number, tr: number, br: number, bl: number}} */
+        let radiusObj;
+        if (typeof radius === 'number') {
+            radiusObj = {
+                tl: radius,
+                tr: radius,
+                br: radius,
+                bl: radius,
+            };
+        } else radiusObj = radius;
+
+        skew = skew || { tl: { x: 0, y: 0 }, tr: { x: 0, y: 0 } };
+
         ctx.beginPath();
-        ctx.moveTo(x + radiusObj.tl, y);
-        ctx.lineTo(x + width - radiusObj.tr, y);
-        ctx.quadraticCurveTo(x + width, y, x + width, y + radiusObj.tr);
-        ctx.lineTo(x + width, y + height - radiusObj.br);
+        ctx.moveTo(x + radiusObj.tl + skew.tl.x, y + skew.tl.y); // Top left
+        ctx.lineTo(x + width - radiusObj.tr + skew.tr.x, y + skew.tr.y); // Top right
+        ctx.quadraticCurveTo(x + width + skew.tr.x, y + skew.tr.y, x + width + skew.tr.x, y + radiusObj.tr + skew.tr.y);
+        ctx.lineTo(x + width, y + height - radiusObj.br); // Bottom right
         ctx.quadraticCurveTo(x + width, y + height, x + width - radiusObj.br, y + height);
-        ctx.lineTo(x + radiusObj.bl, y + height);
+        ctx.lineTo(x + radiusObj.bl, y + height); // Bottom left
         ctx.quadraticCurveTo(x, y + height, x, y + height - radiusObj.bl);
-        ctx.lineTo(x, y + radiusObj.tl);
-        ctx.quadraticCurveTo(x, y, x + radiusObj.tl, y);
+        ctx.lineTo(x + skew.tl.x, y + radiusObj.tl + skew.tl.y); // Top left
+        ctx.quadraticCurveTo(x + skew.tl.x, y + skew.tl.y, x + radiusObj.tl + skew.tl.x, y + skew.tl.y);
         ctx.closePath();
         if (fill) {
             ctx.fill();
@@ -190,7 +238,8 @@ class WalletFile {
 WalletFile.PHI = 1.618;
 WalletFile.WIDTH = 300 * WalletFile.PHI;
 WalletFile.HEIGHT = WalletFile.WIDTH * WalletFile.PHI;
-WalletFile.IDENTICON_SIZE = WalletFile.WIDTH / WalletFile.PHI;
+WalletFile.RADIUS = 16;
+WalletFile.IDENTICON_SIZE = WalletFile.WIDTH / WalletFile.PHI / 1.5;
 WalletFile.QR_SIZE = WalletFile.WIDTH * (1 - 1 / WalletFile.PHI);
 WalletFile.PADDING = 8;
 WalletFile.FONT_FAMILY = '\'Muli\', system-ui, sans-serif';
