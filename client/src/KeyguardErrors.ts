34ec7955d1f402b03aeb4abec8f72ef7d6ac7456
Sebastian <sebastian@nimiq.com>
2018-12-01 18:16:49 -0600

    ErrorMessages and -Types in Client

---
+++ client/src/KeyguardErrors.ts
@@ -0,0 +1,12 @@
+export enum KeyguardErrorTypes {
+    INVALID_REQUEST = 'InvalidRequest', // these are used for request parsing errors.
+    CORE = 'Core', // these are coming from core
+    KEYGUARD = 'Keyguard', // these are used for internal keyguard Errors.
+    UNCLASSIFIED = 'Unclassified',
+}
+
+export enum KeyguardErrorMessages {
+    GOTO_CREATE =  'GOTO_CREATE',
+    CANCEL = 'CANCEL',
+    KEY_ID_NOT_FOUND = 'keyId not found',
+}
