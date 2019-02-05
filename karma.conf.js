c6f1a53b569bdf9c2325c63372f5b87ec23a9420
sisou <hello@soerenschwert.de>
2019-02-04 20:28:34 -0600

    Update Nimiq lib to the master branch
    Take types from the @nimiq/core package, adapting to fixed KDF function.

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (/** @type {any} */ config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'https://cdn.nimiq-network.com/branches/soeren-imagewallet-kdf/web-offline.js',
+            'https://cdn.nimiq-network.com/branches/master/web-offline.js',
             'src/**/*js',
             'tests/**/*.spec.js'
         ],
