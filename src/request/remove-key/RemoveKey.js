0d331f9ecd4c1af59cd728ebf6126f5edb0b1b48
Don Maxito <maxharz@gmail.com>
2019-02-08 14:55:12 -0600

    remove-key: move html to index.html

--- src/request/remove-key/RemoveKey.js
+++ src/request/remove-key/RemoveKey.js
@@ -23,8 +23,7 @@ class RemoveKey {
         this._exportFileHandler = new ExportFile(request, this.run.bind(this), this._reject.bind(this));
 
         /** @type {HTMLElement} */
-        const $removeKey = document.getElementById(RemoveKey.Pages.REMOVE_KEY)
-                        || this._buildRemoveKey();
+        const $removeKey = (document.getElementById(RemoveKey.Pages.REMOVE_KEY));
 
         // remove key
         /** @type {HTMLButtonElement} */
@@ -63,54 +62,6 @@ class RemoveKey {
             success: true,
         };
         this._resolve(result);
-    }
-
-    _buildRemoveKey() {
-        const $el = document.createElement('div');
-        $el.id = RemoveKey.Pages.REMOVE_KEY;
-        $el.classList.add('page', 'nq-card');
-        $el.innerHTML = `
-        <div class="page-header nq-card-header">
-            <h1 data-i18n="remove-key-log-out" class="nq-h1">Don't lose access</h1>
-        </div>
-
-        <div class="page-body nq-card-body">
-            <div class="row">
-                <p data-i18n="remove-key-intro-text" class="nq-text nq-red">
-                    If you log out without saving your account, you will irretrievably lose access to it!
-                </p>
-            </div>
-            <div class="flex-grow"></div>
-            <div>
-                <button id="show-download-key-file" data-i18n="remove-key-download-key-file" class="nq-button-s">
-                    Download Wallet File
-                </button>
-            </div>
-            <div class="flex-grow"></div>
-            <div>
-                <img src="../../assets/sample-recovery-words.png" alt="sample recovery words" />
-                <button id="show-recovery-words" data-i18n="remove-key-show-recovery-words" class="nq-button-s">
-                    Show Recovery Words
-                </button>
-            </div>
-            <div class="flex-grow"></div>
-        </div>
-
-        <div class="page-footer nq-card-footer">
-            <button id="remove-key-first-confirm" class="hide-for-final-confirm nq-button red"
-                data-i18n="remove-key-first-confirm">
-                I am able to log in again
-            </button>
-            <button id="remove-key-final-confirm" class="final-confirm nq-button red" 
-                data-i18n="remove-key-final-confirm">
-                Log out of your account
-            </button>
-        </div>
-        `;
-        /** @type {HTMLElement} */
-        const $app = (document.getElementById('app'));
-        $app.insertBefore($el, $app.children[1]);
-        return $el;
     }
 }
 
