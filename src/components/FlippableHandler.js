e731a1fcf5ca974be4f37e6060027961793959c5
Sebastian <sebastian@nimiq.com>
2019-02-12 12:17:02 -0600

    FlippableHandler constructor adds event handlers only once

--- src/components/FlippableHandler.js
+++ src/components/FlippableHandler.js
@@ -5,24 +5,27 @@ class FlippableHandler { // eslint-disable-line no-unused-vars
      * @param {string} [classname = "dark"] - Pages with this classname will be on the backside of the flip.
      */
     constructor(classname = 'dark') {
-        /** @type {HTMLElement} */
-        const $rotationContainer = (document.getElementById('rotation-container'));
-        window.addEventListener('hashchange', event => {
-            const newHash = new URL(event.newURL).hash;
-            const oldHash = new URL(event.oldURL).hash;
-            if (oldHash && newHash) {
-                const oldPageElement = document.querySelector(oldHash);
-                const newPageElement = document.querySelector(newHash);
-                if (newPageElement
-                    && oldPageElement
-                    && newPageElement.classList.contains(classname) !== oldPageElement.classList.contains(classname)) {
-                    newPageElement.classList.add('display-flex');
-                    window.setTimeout(() => newPageElement.classList.remove('display-flex'), 600);
-                    oldPageElement.classList.add('display-flex');
-                    window.setTimeout(() => oldPageElement.classList.remove('display-flex'), 300);
-                    $rotationContainer.classList.toggle('flipped');
+        if (!FlippableHandler.flippableHandlerInitialised) {
+            /** @type {HTMLElement} */
+            const $rotationContainer = (document.getElementById('rotation-container'));
+            window.addEventListener('hashchange', event => {
+                const newHash = new URL(event.newURL).hash;
+                const oldHash = new URL(event.oldURL).hash;
+                if (oldHash && newHash) {
+                    const $oldEl = document.querySelector(oldHash);
+                    const $newEl = document.querySelector(newHash);
+                    if ($newEl && $oldEl
+                        && $newEl.classList.contains(classname) !== $oldEl.classList.contains(classname)) {
+                        $newEl.classList.add('display-flex');
+                        window.setTimeout(() => $newEl.classList.remove('display-flex'), 600);
+                        $oldEl.classList.add('display-flex');
+                        window.setTimeout(() => $oldEl.classList.remove('display-flex'), 300);
+                        $rotationContainer.classList.toggle('flipped');
+                    }
                 }
-            }
-        });
+            });
+            FlippableHandler.flippableHandlerInitialised = true;
+        }
     }
 }
+FlippableHandler.flippableHandlerInitialised = false;
