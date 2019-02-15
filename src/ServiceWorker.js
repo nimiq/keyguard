745fb46bf6fb0d47fbb3ea6ba652e716b247c077
Don Maxito <maxharz@gmail.com>
2019-02-14 22:20:34 -0600

    add note to license

--- src/ServiceWorker.js
+++ src/ServiceWorker.js
@@ -1,4 +1,5 @@
 /*
+ Based on code under the following license:
  Copyright 2016 Google Inc. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
