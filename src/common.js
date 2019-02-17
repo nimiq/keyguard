94e41dca70b5fbef5bd18f569345af50e6599577
Don Maxito <maxharz@gmail.com>
2019-02-17 14:56:18 -0600

    use console.debug(), copy serviceworker to dist

--- src/common.js
+++ src/common.js
@@ -18,7 +18,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('../../ServiceWorker.js', {
         scope: '../../',
     }).then(reg => {
-        console.log(`Service worker has been registered for scope: ${reg.scope}`);
+        console.debug(`Service worker has been registered for scope: ${reg.scope}`);
     });
 }
 
