d995feb61c2fe35f84ebf09725080f26de239886
sisou <hello@soerenschwert.de>
2019-02-18 18:02:44 -0600

    Extract Base64, JSONUtils, fix types in RpcServer

---
+++ src/lib/JSONUtils.js
@@ -0,0 +1,71 @@
+/* global Base64 */
+/* global Errors */
+
+class JSONUtils {
+    /**
+     * @param {any} value
+     * @returns {string}
+     */
+    static stringify(value) {
+        return JSON.stringify(value, JSONUtils._jsonifyType);
+    }
+
+    /**
+     * @param {string} value
+     * @returns {any}
+     */
+    static parse(value) {
+        return JSON.parse(value, JSONUtils._parseType);
+    }
+
+    /**
+     * @param {string} key
+     * @param {any} value
+     * @returns {any}
+     */
+    static _parseType(key, value) {
+        /* eslint-disable no-prototype-builtins */
+        if (value && value.hasOwnProperty
+            && value.hasOwnProperty(JSONUtils.TYPE_SYMBOL)
+            && value.hasOwnProperty(JSONUtils.VALUE_SYMBOL)) {
+            switch (value[JSONUtils.TYPE_SYMBOL]) {
+                case JSONUtils.UINT8_ARRAY:
+                    return Base64.decode(value[JSONUtils.VALUE_SYMBOL]);
+                default:
+                    throw new Errors.KeyguardError(`Unknown type ${value[JSONUtils.TYPE_SYMBOL]}`);
+            }
+        }
+        return value;
+        /* eslint-enable no-prototype-builtins */
+    }
+
+    /**
+     * @param {string} key
+     * @param {any} value
+     * @returns {any}
+     */
+    static _jsonifyType(key, value) {
+        if (value instanceof Uint8Array) {
+            return JSONUtils._typedObject(JSONUtils.UINT8_ARRAY, Base64.encode(value));
+        }
+        return value;
+    }
+
+    /* eslint-disable-next-line valid-jsdoc */
+    /**
+     * @param {string} type
+     * @param {string} value
+     * @returns {{[x: string]: string}}
+     */
+    static _typedObject(type, value) {
+        /** @type {{[x: string]: string}} */
+        const obj = {};
+        obj[JSONUtils.TYPE_SYMBOL] = type;
+        obj[JSONUtils.VALUE_SYMBOL] = value;
+        return obj;
+    }
+}
+
+JSONUtils.TYPE_SYMBOL = '__';
+JSONUtils.VALUE_SYMBOL = 'v';
+JSONUtils.UINT8_ARRAY = 'UINT8_ARRAY';
--- src/lib/RpcServer.es.js
+++ src/lib/RpcServer.es.js
@@ -1,5 +1,5 @@
-// @ts-nocheck
-/* eslint-disable */
+/* global Base64 */
+/* global JSONUtils */
 
 /**
  * This file was generated from the @nimiq/rpc package source, with `RpcServer` being the only target.
@@ -12,156 +12,18 @@
  *   https://github.com/nimiq/keyguard-next/commits/master/src/lib/RpcServer.es.js
  */
 
-var ResponseStatus;
-(function (ResponseStatus) {
-    ResponseStatus["OK"] = "ok";
-    ResponseStatus["ERROR"] = "error";
-})(ResponseStatus || (ResponseStatus = {}));
+ /** @typedef {{origin: string, data: {id: number, command: string, args: any[]}, returnURL: string?, source?: string|window?}} Message */
 
-/* tslint:disable:no-bitwise */
-class Base64 {
-    static decode(b64) {
-        Base64._initRevLookup();
-        const [validLength, placeHoldersLength] = Base64._getLengths(b64);
-        const arr = new Uint8Array(Base64._byteLength(validLength, placeHoldersLength));
-        let curByte = 0;
-        // if there are placeholders, only get up to the last complete 4 chars
-        const len = placeHoldersLength > 0 ? validLength - 4 : validLength;
-        let i = 0;
-        for (; i < len; i += 4) {
-            const tmp = (Base64._revLookup[b64.charCodeAt(i)] << 18) |
-                (Base64._revLookup[b64.charCodeAt(i + 1)] << 12) |
-                (Base64._revLookup[b64.charCodeAt(i + 2)] << 6) |
-                Base64._revLookup[b64.charCodeAt(i + 3)];
-            arr[curByte++] = (tmp >> 16) & 0xFF;
-            arr[curByte++] = (tmp >> 8) & 0xFF;
-            arr[curByte++] = tmp & 0xFF;
-        }
-        if (placeHoldersLength === 2) {
-            const tmp = (Base64._revLookup[b64.charCodeAt(i)] << 2) |
-                (Base64._revLookup[b64.charCodeAt(i + 1)] >> 4);
-            arr[curByte++] = tmp & 0xFF;
-        }
-        if (placeHoldersLength === 1) {
-            const tmp = (Base64._revLookup[b64.charCodeAt(i)] << 10) |
-                (Base64._revLookup[b64.charCodeAt(i + 1)] << 4) |
-                (Base64._revLookup[b64.charCodeAt(i + 2)] >> 2);
-            arr[curByte++] = (tmp >> 8) & 0xFF;
-            arr[curByte /*++ not needed*/] = tmp & 0xFF;
-        }
-        return arr;
-    }
-    static encode(uint8) {
-        const length = uint8.length;
-        const extraBytes = length % 3; // if we have 1 byte left, pad 2 bytes
-        const parts = [];
-        const maxChunkLength = 16383; // must be multiple of 3
-        // go through the array every three bytes, we'll deal with trailing stuff later
-        for (let i = 0, len2 = length - extraBytes; i < len2; i += maxChunkLength) {
-            parts.push(Base64._encodeChunk(uint8, i, (i + maxChunkLength) > len2 ? len2 : (i + maxChunkLength)));
-        }
-        // pad the end with zeros, but make sure to not forget the extra bytes
-        if (extraBytes === 1) {
-            const tmp = uint8[length - 1];
-            parts.push(Base64._lookup[tmp >> 2] +
-                Base64._lookup[(tmp << 4) & 0x3F] +
-                '==');
-        }
-        else if (extraBytes === 2) {
-            const tmp = (uint8[length - 2] << 8) + uint8[length - 1];
-            parts.push(Base64._lookup[tmp >> 10] +
-                Base64._lookup[(tmp >> 4) & 0x3F] +
-                Base64._lookup[(tmp << 2) & 0x3F] +
-                '=');
-        }
-        return parts.join('');
-    }
-    static _initRevLookup() {
-        if (Base64._revLookup.length !== 0)
-            return;
-        Base64._revLookup = [];
-        for (let i = 0, len = Base64._lookup.length; i < len; i++) {
-            Base64._revLookup[Base64._lookup.charCodeAt(i)] = i;
-        }
-        // Support decoding URL-safe base64 strings, as Node.js does.
-        // See: https://en.wikipedia.org/wiki/Base64#URL_applications
-        Base64._revLookup['-'.charCodeAt(0)] = 62;
-        Base64._revLookup['_'.charCodeAt(0)] = 63;
-    }
-    static _getLengths(b64) {
-        const length = b64.length;
-        if (length % 4 > 0) {
-            throw new Error('Invalid string. Length must be a multiple of 4');
-        }
-        // Trim off extra bytes after placeholder bytes are found
-        // See: https://github.com/beatgammit/base64-js/issues/42
-        let validLength = b64.indexOf('=');
-        if (validLength === -1)
-            validLength = length;
-        const placeHoldersLength = validLength === length ? 0 : 4 - (validLength % 4);
-        return [validLength, placeHoldersLength];
-    }
-    static _byteLength(validLength, placeHoldersLength) {
-        return ((validLength + placeHoldersLength) * 3 / 4) - placeHoldersLength;
-    }
-    static _tripletToBase64(num) {
-        return Base64._lookup[num >> 18 & 0x3F] +
-            Base64._lookup[num >> 12 & 0x3F] +
-            Base64._lookup[num >> 6 & 0x3F] +
-            Base64._lookup[num & 0x3F];
-    }
-    static _encodeChunk(uint8, start, end) {
-        const output = [];
-        for (let i = start; i < end; i += 3) {
-            const tmp = ((uint8[i] << 16) & 0xFF0000) +
-                ((uint8[i + 1] << 8) & 0xFF00) +
-                (uint8[i + 2] & 0xFF);
-            output.push(Base64._tripletToBase64(tmp));
-        }
-        return output.join('');
-    }
-}
-Base64._lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-Base64._revLookup = [];
-
-var ExtraJSONTypes;
-(function (ExtraJSONTypes) {
-    ExtraJSONTypes[ExtraJSONTypes["UINT8_ARRAY"] = 0] = "UINT8_ARRAY";
-})(ExtraJSONTypes || (ExtraJSONTypes = {}));
-class JSONUtils {
-    static stringify(value) {
-        return JSON.stringify(value, JSONUtils._jsonifyType);
-    }
-    static parse(value) {
-        return JSON.parse(value, JSONUtils._parseType);
-    }
-    static _parseType(key, value) {
-        if (value && value.hasOwnProperty &&
-            value.hasOwnProperty(JSONUtils.TYPE_SYMBOL) && value.hasOwnProperty(JSONUtils.VALUE_SYMBOL)) {
-            switch (value[JSONUtils.TYPE_SYMBOL]) {
-                case ExtraJSONTypes.UINT8_ARRAY:
-                    return Base64.decode(value[JSONUtils.VALUE_SYMBOL]);
-            }
-        }
-        return value;
-    }
-    static _jsonifyType(key, value) {
-        if (value instanceof Uint8Array) {
-            return JSONUtils._typedObject(ExtraJSONTypes.UINT8_ARRAY, Base64.encode(value));
-        }
-        return value;
-    }
-    static _typedObject(type, value) {
-        const obj = {};
-        obj[JSONUtils.TYPE_SYMBOL] = type;
-        obj[JSONUtils.VALUE_SYMBOL] = value;
-        return obj;
-    }
-}
-JSONUtils.TYPE_SYMBOL = '__';
-JSONUtils.VALUE_SYMBOL = 'v';
+const ResponseStatus = {
+    OK: 'ok',
+    ERROR: 'error',
+};
 
 class UrlRpcEncoder {
+    /**
+     * @param {Location} url
+     * @returns {Message?}
+     */
     static receiveRedirectCommand(url) {
         // Need referrer for origin check
         if (!document.referrer)
@@ -179,30 +41,38 @@ class UrlRpcEncoder {
         if (!params.has('returnURL'))
             return null;
         // Only allow returning to same origin
-        const returnURL = new URL(params.get('returnURL'));
+        const returnURL = new URL(/** @type {string} */ (params.get('returnURL')));
         if (returnURL.origin !== referrer.origin)
             return null;
         // Parse args
         let args = [];
         if (params.has('args')) {
             try {
-                args = JSONUtils.parse(params.get('args'));
+                args = JSONUtils.parse(/** @type {string} */ (params.get('args')));
             }
             catch (e) {
                 // Do nothing
             }
         }
         args = Array.isArray(args) ? args : [];
-        return {
+
+        return ({
             origin: referrer.origin,
             data: {
-                id: parseInt(params.get('id'), 10),
-                command: params.get('command'),
+                id: parseInt(/** @type {string} */ (params.get('id')), 10),
+                command: /** @type {string} */ (params.get('command')),
                 args,
             },
             returnURL: params.get('returnURL'),
-        };
+        });
     }
+
+    /**
+     * @param {State} state
+     * @param {string} status
+     * @param {any} result
+     * @returns {string}
+     */
     static prepareRedirectReply(state, status, result) {
         const params = new URLSearchParams();
         params.set('status', status);
@@ -214,25 +84,43 @@ class UrlRpcEncoder {
 }
 
 class State {
+    /** @type {number} */
     get id() {
         return this._id;
     }
+
+    /** @type {string} */
     get origin() {
         return this._origin;
     }
+
+    /** @type {any} */
     get data() {
         return this._data;
     }
+
+    /** @type {string?} */
     get returnURL() {
         return this._returnURL;
     }
+
+    /** @type {string|window?} */
     get source() {
         return this._source;
     }
+
+    /**
+     * @param {string} json
+     * @returns {State}
+     */
     static fromJSON(json) {
-        const obj = JSON.parse(json);
+        const obj = JSONUtils.parse(json);
         return new State(obj);
     }
+
+    /**
+     * @param {Message} message
+     */
     constructor(message) {
         if (!message.data.id)
             throw Error('Missing id');
@@ -243,7 +131,12 @@ class State {
         this._data = message.data;
         this._source = 'source' in message ? message.source : null;
     }
+
+    /**
+     * @returns {string}
+     */
     toJSON() {
+        /** @type {{origin: string, data: any, source?: string?, returnURL?: string?}} */
         const obj = {
             origin: this._origin,
             data: this._data,
@@ -264,6 +157,11 @@ class State {
         }
         return JSON.stringify(obj);
     }
+
+    /**
+     * @param {string} status
+     * @param {any} result
+     */
     reply(status, result) {
         console.debug('RpcServer REPLY', result);
         if (status === ResponseStatus.ERROR) {
@@ -305,38 +203,65 @@ class State {
 }
 
 class RpcServer {
+    /**
+     * @param {State} state
+     * @param {any} result
+     */
     static _ok(state, result) {
         state.reply(ResponseStatus.OK, result);
     }
+
+    /**
+     * @param {State} state
+     * @param {Error} error
+     */
     static _error(state, error) {
         state.reply(ResponseStatus.ERROR, error);
     }
+
+    /**
+     * @param {string} allowedOrigin
+     */
     constructor(allowedOrigin) {
         this._allowedOrigin = allowedOrigin;
         this._responseHandlers = new Map();
         this._responseHandlers.set('ping', () => 'pong');
         this._receiveListener = this._receive.bind(this);
     }
+
+    /**
+     * @param {string} command
+     * @param {Function} fn
+     */
     onRequest(command, fn) {
         this._responseHandlers.set(command, fn);
     }
+
     init() {
         window.addEventListener('message', this._receiveListener);
         this._receiveRedirect();
     }
+
     close() {
         window.removeEventListener('message', this._receiveListener);
     }
+
     _receiveRedirect() {
         const message = UrlRpcEncoder.receiveRedirectCommand(window.location);
         if (message) {
             this._receive(message);
         }
     }
+
+    /**
+     * @param {Message} message
+     */
     _receive(message) {
-        let state = null;
+        let _state = null;
         try {
-            state = new State(message);
+            _state = new State(message);
+            const state = _state;
+
             // Cannot reply to a message that has no source window or return URL
             if (!('source' in message) && !('returnURL' in message))
                 return;
@@ -376,8 +301,8 @@ class RpcServer {
             }
         }
         catch (error) {
-            if (state) {
-                RpcServer._error(state, error);
+            if (_state) {
+                RpcServer._error(_state, error);
             }
         }
     }
