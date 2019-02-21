44fd9c90d5371c019090eaa3bcc92fafb66969c1
sisou <hello@soerenschwert.de>
2019-02-20 23:05:48 -0600

    Cancel long-touch upon second concurrent touchstart

--- src/components/DownloadLoginFile.js
+++ src/components/DownloadLoginFile.js
@@ -183,6 +183,7 @@ class DownloadLoginFile extends Nimiq.Observable {
             await new Promise((resolve, reject) => {
                 // Consider the long-touch successfull after LONG_TOUCH_DURATION
                 window.setTimeout(resolve, DownloadLoginFile.LONG_TOUCH_DURATION);
+                this.$loginfile.addEventListener('touchstart', reject, { once: true }); // Second finger cancels overlay
                 this.$loginfile.addEventListener('touchend', reject, { once: true });
             });
 
--- src/components/PassphraseBox.js
+++ src/components/PassphraseBox.js
@@ -60,7 +60,7 @@ class PassphraseBox extends Nimiq.Observable {
             'passphrasebox-log-in': '<button class="submit" data-i18n="passphrasebox-log-in">Unlock</button>',
             'passphrasebox-log-out': '<button class="submit" data-i18n="passphrasebox-log-out">Confirm logout</button>',
             'passphrasebox-confirm-tx': '<button class="submit" data-i18n="passphrasebox-confirm-tx">Confirm transaction</button>',
-            'passphrasebox-download': '<button class="submit" data-i18n="passphrasebox-download">Download Key File</button>',
+            'passphrasebox-download': '<button class="submit" data-i18n="passphrasebox-download">Download Login File</button>',
         };
         /* eslint-enable max-len */
 
