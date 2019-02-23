f3894c83a5f4c4d441cd9586f7e9c9734be34c86
Sebastian <sebastian@nimiq.com>
2019-02-23 15:24:16 -0600

    Some styling for recovery words, undo fixed page height

--- src/components/RecoveryWords.js
+++ src/components/RecoveryWords.js
@@ -73,7 +73,7 @@ class RecoveryWords extends Nimiq.Observable {
                 $wordNumber.innerText = `${i <= 8 ? '0' : ''}${i + 1}`;
 
                 const word = document.createElement('div');
-                word.classList.add('word');
+                word.classList.add('word', 'complete');
                 word.classList.add('recovery-words-input-field');
                 word.appendChild($wordNumber);
                 word.appendChild(content);
