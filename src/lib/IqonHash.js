8745a4b6e11240458f82ee1543c2826afe4ba40e
sisou <hello@soerenschwert.de>
2019-02-27 14:26:26 -0600

    Extract Iqons hash function into own file
    And add fix of hash function from Iqons repo

---
+++ src/lib/IqonHash.js
@@ -0,0 +1,64 @@
+class IqonHash {
+    /**
+     * @param {string} address
+     * @returns {number}
+     */
+    static getBackgroundColorIndex(address) {
+        const hash = this.hash(address);
+        return parseInt(hash[2], 10);
+    }
+
+    /**
+     * @param {string} text
+     * @returns {string}
+     */
+    static hash(text) {
+        const fullHash = (`${text
+            .split('')
+            .map(c => Number(c.charCodeAt(0)) + 3)
+            .reduce((a, e) => a * (1 - a) * this._chaosHash(e), 0.5)}`)
+            .split('')
+            .reduce((a, e) => e + a, '');
+
+        const hash = fullHash
+            .replace('.', fullHash[5]) // Replace the dot as it cannot be parsed to int
+            .substr(4, 17);
+
+        // The index 5 of `fullHash` is currently unused (index 1 of `hash`,
+        // after cutting off the first 4 elements). Iqons.svg() is not using it.
+
+        // A small percentage of returned values are actually too short,
+        // leading to an invalid bottom index and feature color. Adding
+        // padding creates a bottom feature and accent color where no
+        // existed previously, thus it's not a disrupting change.
+        return this._padEnd(hash, 13, fullHash[5]);
+    }
+
+    /**
+     * @param {number} number
+     * @returns {number}
+     */
+    static _chaosHash(number) {
+        const k = 3.569956786876;
+        let an = 1 / number;
+        for (let i = 0; i < 100; i++) {
+            an = (1 - an) * an * k;
+        }
+        return an;
+    }
+
+    /**
+     * Polyfill for String.padEnd()
+     *
+     * @param {string} string
+     * @param {number} maxLength
+     * @param {string} fillString
+     */
+    static _padEnd(string, maxLength, fillString) {
+        if (!!String.prototype.padEnd) return string.padEnd(maxLength, fillString);
+        else {
+            while (string.length < maxLength) string += fillString;
+            return string.substring(0, Math.max(string.length, maxLength));
+        }
+    }
+}
