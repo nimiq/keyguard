b03c013cbacfda64d69160266edb8f006c425913
Sören Schwert <hello@soerenschwert.de>
2019-05-07 16:23:55 +0200

    Move sha256 function up the file, hard fail when no suitable hash function found

--- tools/build.sh
+++ tools/build.sh
@@ -7,6 +7,19 @@ output() {
     echo "$(tput bold) $1$(tput sgr0)"
 }
 
+# Abstraction, because e.g. MacOS does not have sha256sum by default
+sha256() {
+    # The hash command is a simple way to detect if a command is available in the $PATH
+    if hash sha256sum 2>/dev/null; then # Linux
+        sha256sum "$@"
+    elif hash shasum 2>/dev/null; then # MacOS
+        shasum -a 256 "$@"
+    else
+        echo "No suitable sha256 hash function found!"
+        exit 1
+    fi
+}
+
 # execute config file given as parameter; default to testnet
 BUILD=testnet
 if [ "$1" != "" ]; then
@@ -47,13 +60,6 @@ hashsums=\
  154b1251428363c8658c99acbf55b31eef177c0d447767a506952924a37494a9  node_modules/@nimiq/core-web/worker-js.js
  5670830478ac20a634b1436cd24cc3ba2eda23e8f8a7a30f54958920046435ce  node_modules/@nimiq/core-web/worker.js"
 
-sha256() {
-    if hash sha256sum 2>/dev/null; then
-        sha256sum "$@"
-    else
-        shasum -a 256 "$@"
-    fi
-}
 echo "$hashsums" | sha256 --check
 
 if [ ! $? -eq 0 ]; then
