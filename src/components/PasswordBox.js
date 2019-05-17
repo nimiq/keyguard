6132720f3ff4c93d0eb57094278b548e65be2d7a
sisou <hello@soerenschwert.de>
2019-05-17 02:06:39 +0200

    Replace loading-spinner sprites by the real SVG
    Because Safari cannot target SVG sprites shadow-DOM with CSS selectors, prohibiting the loader animation.

--- src/components/PasswordBox.js
+++ src/components/PasswordBox.js
@@ -71,6 +71,7 @@ class PasswordBox extends Nimiq.Observable {
 
         if (!buttonVersions[options.buttonI18nTag]) throw new Error('PasswordBox button i18n tag not defined');
 
+        /* eslint-disable max-len */
         $el.innerHTML = TemplateTags.hasVars(1)`
             <a class="cancel">
                 <svg class="nq-icon">
@@ -80,10 +81,13 @@ class PasswordBox extends Nimiq.Observable {
             <div class="prompt nq-text-s" data-i18n="passwordbox-enter-password">Enter your password</div>
             <div password-input></div>
             ${buttonVersions[options.buttonI18nTag]}
-            <svg height="48" width="54" color="inherit" class="loading-spinner">
-                <use xlink:href="#loading-spinner" />
-            </svg>
+            <!-- Loading spinner SVG -->
+            <svg height="48" width="54" color="inherit" class="loading-spinner"><g id="loading-spinner">
+                <path class="big-hex" d="M51.9,21.9L41.3,3.6c-0.8-1.3-2.2-2.1-3.7-2.1H16.4c-1.5,0-2.9,0.8-3.7,2.1L2.1,21.9c-0.8,1.3-0.8,2.9,0,4.2 l10.6,18.3c0.8,1.3,2.2,2.1,3.7,2.1h21.3c1.5,0,2.9-0.8,3.7-2.1l10.6-18.3C52.7,24.8,52.7,23.2,51.9,21.9z" stroke="currentColor" stroke-width="3" fill="none" stroke-linecap="round" opacity="0.4" stroke-dasharray="92.5 60"/>
+                <path class="small-hex" d="M51.9,21.9L41.3,3.6c-0.8-1.3-2.2-2.1-3.7-2.1H16.4c-1.5,0-2.9,0.8-3.7,2.1L2.1,21.9c-0.8,1.3-0.8,2.9,0,4.2 l10.6,18.3c0.8,1.3,2.2,2.1,3.7,2.1h21.3c1.5,0,2.9-0.8,3.7-2.1l10.6-18.3C52.7,24.8,52.7,23.2,51.9,21.9z" stroke="currentColor" stroke-width="3" fill="none" stroke-linecap="round" stroke-dasharray="47.5 105"/>
+            </g></svg>
         `;
+        /* eslint-enable max-len */
 
         /** @type {HTMLButtonElement} */
         ($el.querySelector('button.submit')).classList.add('nq-button', options.bgColor);
