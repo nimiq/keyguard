da352d1fec34c56e2445fa593a75499261e69847
sisou <hello@soerenschwert.de>
2019-05-19 15:27:21 +0200

    Fix config to redirect to safe, not my

--- src/config/config.testnet.js
+++ src/config/config.testnet.js
@@ -4,5 +4,5 @@
 const CONFIG = { // eslint-disable-line no-unused-vars
     ALLOWED_ORIGIN: 'https://hub.nimiq-testnet.com',
     NETWORK: Constants.NETWORK.TEST,
-    ROOT_REDIRECT: 'https://my.nimiq-testnet.com',
+    ROOT_REDIRECT: 'https://safe.nimiq-testnet.com',
 };
