117390ca901717e76ed7e0c71a1ea6b3722601db
Daniel <github.danimoh@neverbox.com>
2020-04-24 22:35:05 +0200

    NumberFormatting: maxRelativeDeviation option for formatCurrency

--- src/lib/NumberFormatting.js
+++ src/lib/NumberFormatting.js
@@ -1,3 +1,5 @@
+/* global I18n */
+
 class NumberFormatting { // eslint-disable-line no-unused-vars
     /**
      * @param {number} value
@@ -23,24 +25,43 @@ class NumberFormatting { // eslint-disable-line no-unused-vars
     /**
      * @param {number} value
      * @param {string} currency
-     * @param {string} [locale = navigator.language]
+     * @param {number} [maxRelativeDeviation = .1]
+     * @param {string} [locale = I18n.language]
      * @returns {string}
      */
-    static formatCurrency(value, currency, locale = navigator.language) {
+    static formatCurrency(value, currency, maxRelativeDeviation = 0.1, locale = I18n.language) {
         const localeWithLatinNumbers = `${locale}-u-nu-latn`;
-        const formatted = value.toLocaleString([localeWithLatinNumbers, 'en-US'], {
+        const formattingOptions = {
             style: 'currency',
             currency,
             currencyDisplay: 'symbol',
             useGrouping: false,
-        });
-        const integerMatch = formatted.match(/\d+/); // first match is the integer part
-        if (!integerMatch) return formatted; // should never happen
-        const integerPart = integerMatch[0];
-        return formatted
-            // Enforce a dot as decimal separator for consistency. Using capturing groups instead of
-            // lookahead/lookbehind to avoid browser support limitations.
-            .replace(/(\d)\D(\d)/, '$1.$2')
-            .replace(integerPart, NumberFormatting.formatNumber(parseInt(integerPart, 10)));
+            // undefined to start with decimal count typical for this currency, e.g. 2 for eur
+            minimumFractionDigits: /** @type number | undefined */ (undefined),
+        };
+        let formatted;
+        let integers;
+        let relativeDeviation;
+
+        do {
+            formatted = value.toLocaleString([localeWithLatinNumbers, 'en-US'], formattingOptions)
+                // Enforce a dot as decimal separator for consistency and parseFloat. Using capturing groups instead of
+                // lookahead/lookbehind to avoid browser support limitations.
+                .replace(/(\d)\D(\d)/, '$1.$2');
+            const partsMatch = formatted.match(/(-)?\D*(\d+)(\.(\d+))?/);
+            if (!partsMatch) return formatted; // should never happen
+            const [/* full match */, sign, /* integers */, decimalsIncludingSeparator, decimals] = partsMatch;
+            integers = partsMatch[2];
+            const formattedNumber = `${sign || ''}${integers}${decimalsIncludingSeparator || ''}`;
+            relativeDeviation = Math.abs((value - Number.parseFloat(formattedNumber)) / value);
+
+            formattingOptions.minimumFractionDigits = decimals ? decimals.length + 1 : 1;
+        } while (relativeDeviation > maxRelativeDeviation
+            && formattingOptions.minimumFractionDigits <= 20 // maximum allowed value for minimumFractionDigits
+        );
+
+        // apply integer grouping
+        if (integers.length <= 4) return formatted;
+        return formatted.replace(integers, NumberFormatting.formatNumber(parseInt(integers, 10)));
     }
 }
