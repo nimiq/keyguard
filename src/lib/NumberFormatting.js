672384d25a57df1f7afb58113cc204f913e89451
sisou <hello@soerenschwert.de>
2020-05-27 14:24:27 +0200

    Fix number formatting
    No more insertion of a thin space in decimals, but not in the integers.

--- src/lib/NumberFormatting.js
+++ src/lib/NumberFormatting.js
@@ -16,7 +16,7 @@ class NumberFormatting { // eslint-disable-line no-unused-vars
         if (Math.abs(value) < 10000) return result;
 
         // Add thin spaces (U+202F) every 3 digits. Stop at the decimal separator if there is one.
-        const regexp = minDecimals > 0 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(\d{3})+$)/g;
+        const regexp = result.includes('.') ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(\d{3})+$)/g;
         return result.replace(regexp, '$1\u202F');
     }
 
