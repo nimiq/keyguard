d86b2686974fc00615d37f3af6e9abfa596a88b7
sisou <hello@soerenschwert.de>
2020-10-19 11:32:08 +0200

    Add bitcoin-bundle to sign-swap request

--- tools/build.sh
+++ tools/build.sh
@@ -266,7 +266,7 @@ for DIR in src/request/*/ ; do
                 if("'$REQUEST'" != "iframe") {
                     print space[1] "<script defer src=\"/request/'${JS_TOPLEVEL_BUNDLE}'\" integrity=\"sha256-'${JS_TOPLEVEL_BUNDLE_HASH}'\"></script>"
                 }
-                if("'$REQUEST'" == "create" || "'$REQUEST'" == "import" || "'$REQUEST'" == "sign-btc-transaction" || "'$REQUEST'" == "derive-btc-xpub") {
+                if("'$REQUEST'" == "create" || "'$REQUEST'" == "import" || "'$REQUEST'" == "derive-btc-xpub" || "'$REQUEST'" == "sign-btc-transaction" || "'$REQUEST'" == "sign-swap") {
                     print space[1] "<script defer src=\"/request/'${JS_BITCOIN_BUNDLE}'\" integrity=\"sha256-'${JS_BITCOIN_BUNDLE_HASH}'\"></script>"
                 }
                 print space[1] "<script defer src=\"/request/'${REQUEST}'/'${JS_BUNDLE_NAME}'\" integrity=\"sha256-'${JS_BUNDLE_HASH}'\"></script>"
