db0fb5efa84df329b614f142d545b9130ee9f08a
sisou <hello@soerenschwert.de>
2020-11-11 12:21:05 +0100

    Extract common Bitcoin parsing functions into BitcoinEnabledTopLevelApi

--- src/request/derive-btc-xpub/DeriveBtcXpubApi.js
+++ src/request/derive-btc-xpub/DeriveBtcXpubApi.js
@@ -1,11 +1,10 @@
 /* global Nimiq */
-/* global TopLevelApi */
+/* global BitcoinEnabledTopLevelApi */
 /* global DeriveBtcXpub */
 /* global Errors */
-/* global BitcoinUtils */
 
-/** @extends {TopLevelApi<KeyguardRequest.DeriveBtcXpubRequest>} */
-class DeriveBtcXpubApi extends TopLevelApi { // eslint-disable-line no-unused-vars
+/** @extends {BitcoinEnabledTopLevelApi<KeyguardRequest.DeriveBtcXpubRequest>} */
+class DeriveBtcXpubApi extends BitcoinEnabledTopLevelApi { // eslint-disable-line no-unused-vars
     /**
      * @param {KeyguardRequest.DeriveBtcXpubRequest} request
      * @returns {Promise<Parsed<KeyguardRequest.DeriveBtcXpubRequest>>}
@@ -25,42 +24,6 @@ class DeriveBtcXpubApi extends TopLevelApi { // eslint-disable-line no-unused-va
         parsedRequest.bitcoinXPubPath = this.parseBitcoinPath(request.bitcoinXPubPath, 'bitcoinXPubPath');
 
         return parsedRequest;
-    }
-
-    /**
-     * @param {unknown} path
-     * @param {string} name - name of the property, used in error case only
-     * @returns {string}
-     */
-    parseBitcoinPath(path, name) {
-        if (!path || typeof path !== 'string') {
-            throw new Errors.InvalidRequestError(`${name} must be a string`);
-        }
-        if (!this.isValidBitcoinPath(path)) {
-            throw new Errors.InvalidRequestError(`${name}: Invalid path`);
-        }
-        try {
-            BitcoinUtils.parseBipFromDerivationPath(path);
-        } catch (error) {
-            throw new Errors.InvalidRequestError(`${name}: Invalid BIP, only BIP49 and BIP84 are supported`);
-        }
-        return path;
-    }
-
-    /**
-     * @param {string} path
-     * @returns {boolean}
-     */
-    isValidBitcoinPath(path) {
-        if (path.match(/^m(\/[0-9]+'?)*$/) === null) return false;
-
-        // Overflow check.
-        const segments = path.split('/');
-        for (let i = 1; i < segments.length; i++) {
-            if (!Nimiq.NumberUtils.isUint32(parseInt(segments[i], 10))) return false;
-        }
-
-        return true;
     }
 
     get Handler() {
