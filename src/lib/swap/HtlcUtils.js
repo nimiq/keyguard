da9133f3f95db04ec8adbbef2201f6a491eba048
sisou <hello@soerenschwert.de>
2020-11-30 11:22:50 +0100

    Remove unnecessary toHex(fromHex()) when decoding BTC HTLCs

--- src/lib/swap/HtlcUtils.js
+++ src/lib/swap/HtlcUtils.js
@@ -64,12 +64,12 @@ class HtlcUtils { // eslint-disable-line no-unused-vars
 
         // Check hash
         if (asm[++i] !== 'OP_SHA256' || asm[i + 2] !== 'OP_EQUALVERIFY') throw error;
-        const hash = Nimiq.BufferUtils.toHex(Nimiq.BufferUtils.fromHex(asm[++i]));
+        const hash = asm[++i];
         ++i;
 
         // Check redeem address
         if (asm[++i] !== 'OP_DUP' || asm[++i] !== 'OP_HASH160') throw error;
-        const redeemAddressBytes = Nimiq.BufferUtils.toHex(Nimiq.BufferUtils.fromHex(asm[++i]));
+        const redeemAddressBytes = asm[++i];
 
         // End redeem branch, start refund branch
         if (asm[++i] !== 'OP_ELSE') {
@@ -86,7 +86,7 @@ class HtlcUtils { // eslint-disable-line no-unused-vars
 
         // Check refund address
         if (asm[++i] !== 'OP_DUP' || asm[++i] !== 'OP_HASH160') throw error;
-        const refundAddressBytes = Nimiq.BufferUtils.toHex(Nimiq.BufferUtils.fromHex(asm[++i]));
+        const refundAddressBytes = asm[++i];
 
         // End refund branch
         if (branchesVerifiedIndividually) {
