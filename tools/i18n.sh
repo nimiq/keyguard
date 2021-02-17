9612ceccaa4500f1a599be514af7d5e0b892e475
Mathéo <matheo.raveux@gmail.com>
2021-02-17 15:50:57 -0600

    Add Croatian

--- tools/i18n.sh
+++ tools/i18n.sh
@@ -8,7 +8,7 @@ set -e
 # operating system.
 export LC_ALL=C
 
-SUPPORTED_LANGUAGES="de en es fr nl ru uk zh"
+SUPPORTED_LANGUAGES="de en es fr nl ru uk zh hr"
 OUTPUT_FILE=src/translations/index.js
 
 echo "/* eslint-disable */
