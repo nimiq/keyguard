nimiq/keyguard-next

Upgrade QrScanner with video scanning capabilities

tests/lib/LoginFile.spec.js
@@ -38,7 +38,7 @@ describe('LoginFile', () => {
         const qrPosition = LoginFile.calculateQrPosition();
 
         QrScanner.WORKER_PATH = '/base/src/lib/QrScannerWorker.js';
-        const decoded = await QrScanner.scanImage($img, qrPosition, null, null, true);
+        const decoded = await QrScanner.scanImage($img, qrPosition, undefined, undefined, true);
         expect(decoded).toEqual(serializedKey);
     });
 });

nimiq/keyguard

Co-authored-by: Daniel <github.danimoh@neverbox.com>

tests/lib/LoginFile.spec.js
@@ -57,8 +57,8 @@ describe('LoginFile', () => {
         const qrPosition = LoginFile.calculateQrPosition();
 
         QrScanner.WORKER_PATH = '/base/src/lib/QrScannerWorker.js';
-        const decoded1 = await QrScanner.scanImage($img1, qrPosition, null, null, true);
-        const decoded2 = await QrScanner.scanImage($img2, qrPosition, null, null, true);
+        const decoded1 = await QrScanner.scanImage($img1, qrPosition, undefined, undefined, true);
+        const decoded2 = await QrScanner.scanImage($img2, qrPosition, undefined, undefined, true);
         expect(decoded1).toEqual(serializedEntropy);
         expect(decoded2).toEqual(serializedEntropyWithLabel);
 