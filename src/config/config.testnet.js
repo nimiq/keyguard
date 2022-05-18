3ff9d483799e19e4b99ad8f893347d85549c3867
Sören <hello@soerenschwert.de>
2022-05-18 20:50:40 +0200

    Update testnet config for v2 deployment URLs

--- src/config/config.testnet.js
+++ src/config/config.testnet.js
@@ -2,10 +2,10 @@
 
 // @ts-ignore
 const CONFIG = { // eslint-disable-line no-unused-vars
-    ALLOWED_ORIGIN: 'https://hub.nimiq-testnet.com',
+    ALLOWED_ORIGIN: 'https://hub.v2.nimiq-testnet.com',
     NETWORK: Constants.NETWORK.TEST,
     BTC_NETWORK: 'TEST', // BitcoinConstants is not included in the common bundle
-    ROOT_REDIRECT: 'https://wallet.nimiq-testnet.com',
+    ROOT_REDIRECT: 'https://wallet.v2.nimiq-testnet.com',
 
     POLYGON_CHAIN_ID: 80001,
     USDC_CONTRACT_ADDRESS: '0x0FA8781a83E46826621b3BC094Ea2A0212e71B23',
