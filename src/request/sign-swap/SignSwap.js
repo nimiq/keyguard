4da9e884c9bf1f05c4457866514bc043e4c7c4dc
Daniel <github.danimoh@neverbox.com>
2022-07-25 12:01:15 +0200

    Handle KYC grant tokens and display swap authorization indicator

--- src/request/sign-swap/SignSwap.js
+++ src/request/sign-swap/SignSwap.js
@@ -352,6 +352,7 @@ class SignSwap {
             hideInput: !request.keyInfo.encrypted,
             buttonI18nTag: 'passwordbox-confirm-swap',
             minLength: request.keyInfo.hasPin ? Key.PIN_LENGTH : undefined,
+            showSwapAuthorization: !!request.kyc,
         });
 
         this._passwordBox.on(
