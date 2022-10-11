28b0debb68b07d74ede57bacb772a1eea6544cfc
Sören <hello@soerenschwert.de>
2022-10-11 15:15:09 +0200

    Add RSA key computation to Key class

--- src/config/config.testnet.js
+++ src/config/config.testnet.js
@@ -6,4 +6,6 @@ const CONFIG = { // eslint-disable-line no-unused-vars
     NETWORK: Constants.NETWORK.TEST,
     BTC_NETWORK: 'TEST', // BitcoinConstants is not included in the common bundle
     ROOT_REDIRECT: 'https://wallet.nimiq-testnet.com',
+
+    RSA_KEY_BITS: 2048, // Possible values are 1024 (fast, but unsafe), 2048 (good compromise), 4096 (slow, but safe)
 };
