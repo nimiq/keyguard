1dc0a0b0fe626e0a0df6a8d1f5d137907d6cef2a
Sören <hello@soerenschwert.de>
2022-10-11 18:26:15 +0200

    Add Rust code compiled to WASM for secret aggregation

--- src/lib/multisig/MultisigUtils.js
+++ src/lib/multisig/MultisigUtils.js
@@ -1,5 +1,6 @@
 /* global Nimiq */
 /* global MerkleTreePatch */
+/* global wasm_bindgen */
 
 class MultisigUtils { // eslint-disable-line no-unused-vars
     /**
@@ -24,11 +25,31 @@ class MultisigUtils { // eslint-disable-line no-unused-vars
     }
 
     /**
+     * Aggregate commitment secrets with scalar multiplication in WASM
      * @param {Uint8Array[]} secrets
      * @param {Uint8Array} bScalar
      * @returns {Promise<Nimiq.RandomSecret>}
      */
-    static async aggregateSecrets(secrets, bScalar) { // eslint-disable-line no-unused-vars
-        throw new Error('NOT IMPLEMENTED!');
+    static async aggregateSecrets(secrets, bScalar) {
+        const scriptId = 'multisig-wasm';
+        if (!document.getElementById(scriptId)) {
+            const script = document.createElement('script');
+            script.id = scriptId;
+            script.src = '../../lib/multisig/wasm/pkg/multisig.js'; // Relative path from a request URL
+            /** @type {Promise<void>} */
+            const loadPromise = new Promise(resolve => script.addEventListener('load', () => resolve()));
+            document.head.appendChild(script);
+            await loadPromise;
+            await wasm_bindgen('../../lib/multisig/wasm/pkg/multisig_bg.wasm'); // Relative path from a request URL
+        }
+
+        const concatenatedSecrets = new Uint8Array(secrets.reduce((sum, secret) => sum + secret.length, 0));
+        let writePos = 0;
+        for (const secret of secrets) {
+            concatenatedSecrets.set(secret, writePos);
+            writePos += secret.length;
+        }
+        const secret = wasm_bindgen.aggregate_secrets(concatenatedSecrets, bScalar);
+        return new Nimiq.RandomSecret(secret);
     }
 }
