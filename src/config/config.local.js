9d8f844c4893ae95436dd201585b00f7132acc1e
Daniel <github.danimoh@neverbox.com>
2022-11-20 21:29:08 -0600

    BrowserStack local iOS testing compatibility
    On iOS, BrowserStack routes localhost connections for testing of local code
    through an actual domain (bs-local.com) because iOS blocks localhost connections
    to content that's not sitting on the phone.
    This commit adds changes to treat bs-local.com just like localhost, such that
    a locally served dev server can be tested with BrowserStack.

--- src/config/config.local.js
+++ src/config/config.local.js
@@ -2,8 +2,10 @@
 
 // We want to only allow this config file in dev environments. Supporting IPs as hostname is nice for debugging e.g.
 // on mobile devices, though. We assume that any keyguard instance hosted in production would be accessed by DNS.
+// Additionally, we whitelist BrowserStack's localhost tunnel bs-local.com for iOS debugging in BrowserStack, see
+// https://www.browserstack.com/docs/live/local-testing/ios-troubleshooting-guide
 const ipRegEx = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
-if (window.location.hostname !== 'localhost' && !ipRegEx.test(window.location.hostname)) {
+if (!/^(?:localhost|bs-local.com)$/.test(window.location.hostname) && !ipRegEx.test(window.location.hostname)) {
     throw new Error('Using development config is only allowed locally');
 }
 
