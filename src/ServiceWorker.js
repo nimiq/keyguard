20babc0d33b1685110463fb82b9f3cb6d0974004
Daniel <github.danimoh@neverbox.com>
2023-02-27 16:07:20 +0100

    ServiceWorker: preserve testnet branch selector cookie

--- src/ServiceWorker.js
+++ src/ServiceWorker.js
@@ -1,8 +1,13 @@
 // eslint-disable-next-line spaced-comment
 /// <reference lib='webworker' />
 
+/**
+ * Simplified definition of the browser's native CookieStore. Not to be confused with our CookieStorage.
+ * @typedef {{get: (name: string) => Promise<{ value: string } | undefined>}} CookieStore
+ */
+
 // eslint-disable-next-line no-restricted-globals
-const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
+const sw = /** @type {ServiceWorkerGlobalScope & { cookieStore?: CookieStore }} */ (/** @type {unknown} */ (self));
 
 /* eslint-disable no-restricted-globals */
 // const PRECACHE = 'v1';
@@ -41,7 +46,8 @@ let clientReferrersCleanupInterval = -1;
 
 // Intercept fetch
 sw.addEventListener('fetch', event => {
-    const requestHost = new URL(event.request.url).host;
+    const request = event.request;
+    const requestHost = new URL(request.url).host;
     if (requestHost !== sw.location.host) return;
 
     if (event.resultingClientId) {
@@ -50,7 +56,7 @@ sw.addEventListener('fetch', event => {
         // document.referrer if a service worker handles the request, regardless of the previous page's, the Keyguard's
         // and this request's referrerPolicy. Possibly related bug tracking:
         // https://bugzilla.mozilla.org/show_bug.cgi?id=1626616, https://bugzilla.mozilla.org/show_bug.cgi?id=1626192.
-        clientReferrers.set(event.resultingClientId, event.request.referrer);
+        clientReferrers.set(event.resultingClientId, request.referrer);
         if (clientReferrersCleanupInterval === -1) {
             clientReferrersCleanupInterval = sw.setInterval(async () => {
                 for (const clientId of clientReferrers.keys()) {
@@ -68,10 +74,20 @@ sw.addEventListener('fetch', event => {
 
     // Strip cookies from all requests with matching host, as those are the ones potentially leaking cookie data.
     // See: https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy#Cross-origin_data_storage_access
-    event.respondWith(fetch(event.request, {
-        // omit cookie transmission
-        credentials: 'omit',
-    }));
+    // On testnet, allow cookies, if the branch selector cookie is set.
+    event.respondWith((async () => {
+        const preserveBranchSelectorCookie = sw.location.hostname.endsWith('.nimiq-testnet.com')
+            // If cookieStore is supported, check whether the branch selector cookie is set. If cookieStore is not
+            // supported, there's no way for us to know whether the branch selector cookie is set, and we have to assume
+            // that it is, which is fine in testnet. Notably, we cannot check request.headers.get('cookie') because the
+            // cookies are only attached by the browser after the ServiceWorker processed the request, depending on
+            // request.credentials.
+            && (!sw.cookieStore || !!(await sw.cookieStore.get('environment')));
+        // Note that if we want to preserve the branch selector cookie, we have to keep all cookies because it is not
+        // possible to set custom request cookies, only including the branch selector cookie, as the cookie header can
+        // not be overwritten: https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
+        return fetch(request, !preserveBranchSelectorCookie ? { credentials: 'omit' } : {});
+    })());
 });
 
 sw.addEventListener('message', async ({ origin, source: client, data: eventData }) => {
